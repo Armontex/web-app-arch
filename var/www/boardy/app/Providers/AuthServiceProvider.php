2b2edc5358ccc264273011852b65fefab7b1ea92
Armontex <windle1337@gmail.com>
2026-05-27 03:33:36 +0500

    lab14: configure passport token ttl

---
+++ var/www/boardy/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        Passport::tokensExpireIn(now()->addMinutes(15));
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+    }
+}
